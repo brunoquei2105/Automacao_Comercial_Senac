0375de90e192c1ddd1a308b130c2da1d97aa2d3a
Bruno Queiroz <bruno.queiroz@liferay.com>
2022-04-21 14:07:25 -0300

    Create project

---
+++ src/main/java/com/senac/ac/AutomacaoComercialApplication.java
@@ -0,0 +1,13 @@
+package com.senac.ac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomacaoComercialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomacaoComercialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/senac/ac/AutomacaoComercialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.ac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomacaoComercialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
